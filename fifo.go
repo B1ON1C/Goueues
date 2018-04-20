c83ba8caab0fcc701e35009cfe837ffe662fb3d1
Luis <B1ON1C@users.noreply.github.com>
2018-04-20 10:32:46 +0200

    Create fifo.go

---
+++ fifo.go
@@ -0,0 +1,32 @@
+package fifo
+
+// Fifo holds info about the object
+type Fifo struct{
+	FifoList []interface{}
+}
+
+// New creates new Fifo pointer and returns it
+func New() *Fifo{
+	return &Fifo{}
+}
+
+// Add add to the *Fifo list new interface
+func(f *Fifo) Add(i interface{}){
+	f.FifoList = append(f.FifoList, i)
+}
+
+// Pop returns the first item of the list *Fifo
+func(f *Fifo) Pop() (i interface{}) {
+	if len(f.FifoList)>0{
+		i = f.FifoList[0]
+		f.FifoList = f.FifoList[1:]
+
+		return
+	}
+	return nil
+}
+
+// Len returns int about the actual size of the *Fifo list
+func(f *Lifo) Len() int{
+	return len(f.LifoList)
+}
